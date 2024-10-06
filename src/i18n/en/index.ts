761b70c7158a99e2c144390582b0f65aeade29a5
Ester Monteiro <esterpmonteiro@gmail.com>
2024-10-06 22:01:30 +0200

    Adding translations

---
+++ src/i18n/en/index.ts
@@ -0,0 +1,50 @@
+const english = {
+  'dashboard.loadingMessage': 'Loading...',
+  'dashboard.errorMessage': 'Error: {{vehiclesError}}',
+  'dashboard.vehiclesTable.noVehiclesMessage': 'Vehicles data not found!',
+  'dashboard.vehiclesDetails.vehicleNotFoundMessage': 'Vehicle not found!',
+  'dashboard.vehiclesDetails.backButtonLabel': 'Back',
+  'dashboard.dataPointValue.on': 'On',
+  'dashboard.dataPointValue.off': 'Off',
+  'dashboard.dataPointTitle.id': 'Vehicle Id',
+  'dashboard.dataPointTitle.speed': 'Speed (km/h)',
+  'dashboard.dataPointTitle.batteryLevel': 'Battery Level (%)',
+  'dashboard.dataPointTitle.tirePressure': 'Tire Pressure (psi)',
+  'dashboard.dataPointTitle.motorEfficiency': 'Motor Efficiency (%)',
+  'dashboard.dataPointTitle.regenerativeBraking': 'Regenerative Braking',
+  'dashboard.dataPointTitle.batteryTemperature': 'Battery Temperature (°C)',
+  'dashboard.dataPointTitle.energyConsumption': 'Energy Consumption (kWh)',
+  'dashboard.dataPointTitle.gpsLocation': 'GPS Location (lat/long) ',
+  'dashboard.dataPointTitle.suspensionStatus': 'Suspension (mm) ',
+  'dashboard.dataPointTitle.odometer': 'Odometer (km)',
+  'dashboard.dataPointTitle.chargingStatus': 'Charging',
+  'dashboard.dataPointTitle.temperature': 'Temperature (°C)',
+  'dashboard.notifications.backButtonLabel': 'Back',
+  'dashboard.notifications.noNotificationsMessage': 'No notifications at the moment',
+  'dashboard.notifications.message.speed.highSpeed':
+    'Speed exceeds 120 km/h. Current speed: <bold>{{value}} km/h</bold>',
+  'dashboard.notifications.message.batteryLevel.lowBattery':
+    'Battery level is below 20%. Current battery level: <bold>{{value}}%</bold>',
+  'dashboard.notifications.message.tirePressure.lowTirePressure':
+    'Tire pressure is too low. Current tire pressure: <bold>{{value}} psi</bold>',
+  'dashboard.notifications.message.tirePressure.highTirePressure':
+    'Tire pressure is too high. Current tire pressure: <bold>{{value}} psi</bold>',
+  'dashboard.notifications.message.motorEfficiency.lowMotorEfficiency':
+    'Motor efficiency is below 70%. Current efficiency: <bold>{{value}}%</bold>',
+  'dashboard.notifications.message.batteryTemperature.highBatteryTemperature':
+    'Battery temperature is too high. Current temperature: <bold>{{value}}°C</bold>',
+  'dashboard.notifications.message.batteryTemperature.lowBatteryTemperature':
+    'Battery temperature is too low. Current temperature: <bold>{{value}}°C</bold>',
+  'dashboard.notifications.message.energyConsumption.highEnergyConsumption':
+    'Energy consumption is too high. Current consumption: <bold>{{value}} kWh/100km</bold>',
+  'dashboard.notifications.message.suspensionStatus.lowSuspensionStatus':
+    'Suspension status is below 60%. Current status: <bold>{{value}}%</bold>',
+  'dashboard.notifications.message.odometer.odometerValue':
+    'Maintenance Alert: Vehicle odometer has reached <bold>{{value}}km</bold>.',
+  'dashboard.notifications.message.temperature.highTemperature':
+    'Temperature is too high. Current temperature: <bold>{{value}}°C</bold>',
+  'dashboard.notifications.message.temperature.lowTemperature':
+    'Temperature is too low. Current temperature: <bold>{{value}}°C</bold>',
+};
+
+export default english;
